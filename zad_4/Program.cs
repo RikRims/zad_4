559ec509479b688fa4cef95d2c35a5bd752c7a10
Rik Rims <Kirill.smirik1@ya.ru>
2023-02-08 17:22:38 +0300

    Добавьте файлы проекта.

---
+++ zad_4/Program.cs
@@ -0,0 +1,25 @@
+﻿//проверка числа на простость
+bool Prosto (int a)
+{
+    if (a == 1) return false;
+    else if ((a / 7 > 1) & (a % 7 == 0)) return false;
+    else if ((a / 5 > 1) & (a % 5 == 0)) return false;
+    else if ((a / 3 > 1) & (a % 3 == 0)) return false;
+    else if ((a / 2 > 1) & (a % 2 == 0)) return false;
+
+    else return true;
+}
+
+int a;
+
+//ввод числа с проверкой
+Console.WriteLine("дай число: ");
+while (!int.TryParse(Console.ReadLine(), out a))
+{
+    Console.WriteLine("Дай число! ");
+}
+
+//вызов функции
+if (Prosto(a))
+    Console.WriteLine("Число простое.");
+else Console.WriteLine("Число не простое.");
